8a37b54f244a68d8ca2aeaec5aaf7d52f69613af
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-05-17 23:12:20 +0800

    feat: modifying ResponseEntityExceptionHandler's default response body

--- src/main/java/com/gabriel/springrestspecialist/api/exception/ApiExceptionHandler.java
+++ src/main/java/com/gabriel/springrestspecialist/api/exception/ApiExceptionHandler.java
@@ -3,46 +3,46 @@ package com.gabriel.springrestspecialist.api.exception;
 import com.gabriel.springrestspecialist.domain.exception.BusinessLogicException;
 import com.gabriel.springrestspecialist.domain.exception.EntityAlreadyInUseException;
 import com.gabriel.springrestspecialist.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(BusinessLogicException.class)
-    public ResponseEntity<?> handleBusinessLogicException(BusinessLogicException e) {
-        return handleException(e, BAD_REQUEST);
+    public ResponseEntity<?> handleBusinessLogicException(BusinessLogicException ex, WebRequest request) {
+        return handleExceptionInternal(ex, BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
-        return handleException(e, NOT_FOUND);
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        return handleExceptionInternal(ex, NOT_FOUND, request);
     }
 
     @ExceptionHandler(EntityAlreadyInUseException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityAlreadyInUseException e) {
-        return handleException(e, CONFLICT);
+    public ResponseEntity<?> handleEntityNotFoundException(EntityAlreadyInUseException ex, WebRequest request) {
+        return handleExceptionInternal(ex, CONFLICT, request);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
-        return handleException(e, UNSUPPORTED_MEDIA_TYPE);
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        body = ApiException.builder()
+            .message(ex.getMessage())
+            .timestamp(LocalDateTime.now())
+            .build();
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
-    private ResponseEntity<?> handleException(Throwable e, HttpStatus status) {
-        var apiException = exceptionResponseBuilder(e.getMessage()).build();
-        return ResponseEntity.status(status).body(apiException);
-    }
-
-    private ApiException.ApiExceptionBuilder exceptionResponseBuilder(String message) {
-        return ApiException.builder()
-            .message(message)
-            .timestamp(LocalDateTime.now());
+    private ResponseEntity<?> handleExceptionInternal(Exception ex, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), status, request);
     }
 }
