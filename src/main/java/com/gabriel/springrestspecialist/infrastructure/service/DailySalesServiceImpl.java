8a3ec0b2d3b2c7be099e52eb343f5f29c0859103
Gabriel M. Fernandes <gabriel.f@compound.direct>
2025-07-10 18:27:44 +0800

    feat: daily sales impl

--- src/main/java/com/gabriel/springrestspecialist/infrastructure/service/DailySalesServiceImpl.java
+++ src/main/java/com/gabriel/springrestspecialist/infrastructure/service/DailySalesServiceImpl.java
@@ -9,8 +9,16 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static com.gabriel.springrestspecialist.domain.model.OrderStatus.CONFIRMED;
+import static com.gabriel.springrestspecialist.domain.model.OrderStatus.DELIVERED;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,9 +31,9 @@ public class DailySalesServiceImpl implements DailySalesService {
         var builder = entityManager.getCriteriaBuilder();
         var query = builder.createQuery(DailySales.class);
         var root = query.from(Order.class);
+        var predicates = new ArrayList<Predicate>();
 
-        var dateOnly = builder.function("DATE_TRUNC", Date.class,
-            builder.literal("day"), root.get("createdAt"));
+        var dateOnly = truncateDate(builder, root, filter);
         var selection = builder.construct(
             DailySales.class,
             dateOnly,
@@ -33,9 +41,36 @@ public class DailySalesServiceImpl implements DailySalesService {
             builder.sum(root.get("grandTotal"))
         );
 
-        query.select(selection);
-        query.groupBy(dateOnly);
+        setFilters(filter, predicates, builder, root);
+        predicates.add(root.get("status").in(CONFIRMED, DELIVERED));
+
+        query.select(selection)
+            .where(predicates.toArray(new Predicate[0]))
+            .groupBy(dateOnly);
 
         return entityManager.createQuery(query).getResultList();
+    }
+
+    private void setFilters(DailySalesFilter filter, ArrayList<Predicate> predicates, CriteriaBuilder builder, Root<Order> root) {
+        if (filter.getRestaurantId() != null) {
+            predicates.add(builder.equal(root.get("restaurant").get("id"), filter.getRestaurantId()));
+        }
+
+        if (filter.getCreatedAtStart() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("createdAt"), filter.getCreatedAtStart()));
+        }
+
+        if (filter.getCreatedAtEnd() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("createdAt"), filter.getCreatedAtEnd()));
+        }
+    }
+
+    private Expression<Date> truncateDate(CriteriaBuilder builder, Root<Order> root, DailySalesFilter filter) {
+        return builder.function(
+            "DATE_TRUNC",
+            Date.class,
+            builder.literal("day"),
+            root.get("createdAt")
+        );
     }
 }
